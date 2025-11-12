efbcac7bb72f0d33e8791708f2e8e3130e49b480
itsjasminn <muvvahhida@gmail.com>
2025-11-12 19:23:04 +0500

    new

--- leetcode.py
+++ leetcode.py
@@ -2269,3 +2269,18 @@ https://leetcode.com/problems/count-of-matches-in-tournament/description/
 
 # n = 7
 # print(n - 1)
+
+
+"""
+https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/description/
+"""
+
+num = 14
+count = 0
+while num > 0:
+    if num % 2 == 0:
+        num //= 2
+    else:
+        num -= 1
+    count += 1
+print(count)
