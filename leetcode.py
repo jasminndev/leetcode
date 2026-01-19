d0e0bd356be0708e6722dec45d65010d0d86bb27
itsjasminn <muvvahhida@gmail.com>
2026-01-19 10:25:33 +0500

    new

--- leetcode.py
+++ leetcode.py
@@ -2375,3 +2375,17 @@ https://leetcode.com/problems/find-the-integer-added-to-array-i/description/
 # print(min2 - min1)
 
 
+#################################################################################
+
+
+#
+# lst = [1, 4, 2, 6, 4, 6]
+#
+# left = 0
+# right = len(lst) - 1
+#
+# while left < right:
+#     print(lst[left], lst[right])
+#
+#     left += 1
+#     right -= 1
